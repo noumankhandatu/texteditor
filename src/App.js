46fa37e2d2b9d4f97d6f04d006c348ed11176f53
noumanakadatu <noumankhan619.915@gmail.com>
2022-03-29 20:11:17 +0500

    conm

--- src/App.js
+++ src/App.js
@@ -2,27 +2,57 @@ import React, { useState } from "react";
 import { CKEditor } from "@ckeditor/ckeditor5-react";
 import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 import parse from "html-react-parser";
+import { MathJax, MathJaxContext } from "better-react-mathjax";
 import "./App.css";
+import { Button } from "@material-ui/core";
 
 function App() {
-  const [data, setData] = useState("");
+  const [text, setText] = useState("");
+  const [blocked, setBlocked] = useState(true);
 
+  const config = {
+    loader: { load: ["[tex]/html"] },
+    tex: {
+      inlineMath: [
+        ["$", "$"],
+        ["\\(", "\\)"],
+      ],
+      displayMath: [
+        ["$$", "$$"],
+        ["\\[", "\\]"],
+      ],
+    },
+  };
+  const handleEditor = (event, editor) => {
+    const text = editor.getData();
+    setText(text);
+  };
+  const handleBlocker = () => {
+    setBlocked(!blocked);
+  };
   return (
     <div className="App">
       <div className="editor">
-        <CKEditor
-          editor={ClassicEditor}
-          data={data}
-          onChange={(event, editor) => {
-            const data = editor.getData();
-            setData(data);
-          }}
-        />
+        <Button variant="contained" onClick={handleBlocker} color="secondary">
+          Toggle To Maths Equations || Normal Editor
+        </Button>
+        <CKEditor editor={ClassicEditor} text={text} onChange={handleEditor} />
       </div>
       <div>
-        <h2>Typin Test</h2>
-        <p>\( ax^2+bx +c=0\)</p>
-        <p>{parse(data)}</p>
+        <h2>Typin Test </h2>
+        {blocked ? (
+          <div>
+            <p>{parse(text)}</p>
+          </div>
+        ) : (
+          <div>
+            <MathJaxContext version={3} config={config}>
+              <div>
+                <MathJax>{`\\( ${text}\\)`}</MathJax>
+              </div>
+            </MathJaxContext>
+          </div>
+        )}
       </div>
     </div>
   );
---
+++ src/math.js
@@ -0,0 +1,16 @@
+// import React, { useState } from "react";
+
+// export default function MathEqn() {
+//   return (
+
+//   );
+// }
+// {
+/* <MathJax hideUntilTypeset={"first"}>
+{` \\(x\\) or \\(\\frac{25x}{10} = 2^{10}\\
+
+\\[\\sum_{n = 100}^{1000}\\left(\\frac{10\\sqrt{n}}{n}\\right)\\]
+... and then continue with inline math.`}
+</MathJax> */
+// }
+
